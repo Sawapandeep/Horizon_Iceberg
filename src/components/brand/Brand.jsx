e0b0ba8c6c00977f74cec9533c433fd9752d4c8e
SAWAPANDEEP <78143025+Sawapandeep@users.noreply.github.com>
2022-12-10 09:37:55 +0530

    removed brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -7,12 +7,12 @@ import { epic, pubg ,steam} from './imports';
   return (
     <div className="Horizon__brand section__padding" id='brand'>
       <div>
-        <a href='https://store.steampowered.com/'
->       <img src={steam} alt="google" style={{width:'252px',height:'80px'}} /></a> 
-<a href='https://store.epicgames.com/'
->       <img src={epic} alt="google" style={{width:'260px',height:'110px'}} /></a> 
+//         <a href='https://store.steampowered.com/'
+// >       <img src={steam} alt="google" style={{width:'252px',height:'80px'}} /></a> 
+// <a href='https://store.epicgames.com/'
+// >       <img src={epic} alt="google" style={{width:'260px',height:'110px'}} /></a> 
         {/* <video muted class="vid" loop src={rocketleague} onMouseOver={"play()"} onMouseOut={"pause()"}/> */}
-      </div> 
+//       </div> 
       <div>
         {/* <video muted class="vid" loop src={pubg}/><img src={steam} alt="onedrive" style={{ width: '82px',height: '20px' }}/> */}
         </div>
