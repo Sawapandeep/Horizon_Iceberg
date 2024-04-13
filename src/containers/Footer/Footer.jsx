0edeac1a6b06dfd1f5666d3b91cacf3e733da0c3
SAWAPANDEEP <78143025+Sawapandeep@users.noreply.github.com>
2024-04-13 20:50:01 +0530

    blank commit
     changes to footer, brand divs

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -39,11 +39,11 @@ import logo from'../../assets/logo.svg';
       <div className="Horizon__footer-links_div">
         <h4>Get in touch</h4>
         <a href="https://www.linkedin.com/in/sawapandeep-singh-6984aa243" target="_blank"
-        className="social">
+        className="social" rel="noreferrer">
         <FontAwesomeIcon icon={faLinkedin} size="2x" />
         </a>
         <a href="https://t.me/Horizon_Iceberg" target="_blank"
-        className="social">
+        className="social" rel="noreferrer">
         <FontAwesomeIcon icon={faTelegram} size="2x" />
         </a>
         <p>085-132567</p>
